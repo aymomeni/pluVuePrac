2bcf91a9c6bc1afcd0c84e1745c560047d7e43de
Ali Momeni <aymomeni@gmail.com>
2019-06-14 11:45:12 -0600

    using props to share data betwen parent and child component

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -20,7 +20,19 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  props: ['parts', 'position'],
+  //props: ['parts', 'position'],
+  props: { 
+    parts: { 
+      type: Array,
+      required: true
+   }, 
+   position: { 
+     type: String,
+     required: true,
+     validator: function(value) {
+       return ['left', 'right', 'top', 'base'].includes(value);
+     }
+     } },
   data() {
     return { selectedPartIndex: 0 };
   },
